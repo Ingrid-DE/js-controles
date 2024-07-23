4220a7aa2e0b2323bf6cb0d09a443bb5ed2234de
Ingrid Dahiana Enriquez Dos Santos <ingridenriquez74@gmail.com>
2024-07-22 21:12:21 -0400

    recuperar-valores

--- js/app.js
+++ js/app.js
@@ -3,11 +3,26 @@
     que primero se cargue TODO el documento
     HTML, por ultimo javascript
 */
-document
-.addEventListener("DOMContentLoaded", function(){
-    alert("Holis");
+document.addEventListener("DOMContentLoaded", function(){
+    // esta es zona segura
+    //console.log(document.getElementById("txtNombre").value);
+    //console.log(document.getElementById("txtApellido").value);
 });
 
+// añadiendo evento clic al botón mostrar
+const btnMostrar = document.getElementById("btnMostrar");
+btnMostrar.addEventListener("click", function() {
+    //console.log('haciendo click');
+    //alert("hice clic");
+    //recuperando info del formulario
+    const nombres = document.getElementById("txtNombre").value;
+    const apellido = document.getElementById("txtApellido").value;
+    const cedula = document.getElementById("txtCedula").value;
+    const dirección = document.getElementById("txtDirección").value;
+    console.log('nombres ' +nombres+ ', apellidos ' +apellido+ ', cedula ' + cedula+ ', dirección ' + dirección);
+    alert('nombres ' +nombres+ ', apellidos ' +apellido+ ', cedula ' + cedula+ ', dirección ' + dirección);
+
+});
 
 function hola(nombre, apellido) {
     console.log(nombre, apellido);
